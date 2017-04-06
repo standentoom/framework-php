3d666608f6d055b6c6e9b2879b33801c9fcea3a3
DESKTOP-EM2NVUS\tooms <99041312@mydavinci.nl>
2017-04-06 09:11:29 +0200

    no message

--- core/config.php
+++ core/config.php
@@ -1,7 +1,7 @@
 <?php
 
 define('DB_TYPE', 'mysql');
-define('DB_HOST', '127.0.0.1:8889');
+define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'lecture');
 define('DB_USER', 'root');
 define('DB_PASS', 'root');
